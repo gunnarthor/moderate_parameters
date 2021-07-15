2e031041f41049e710090882bfaaed023022ad42
Gunnar Thor <gunnar@gunnarthor.com>
2021-07-15 13:10:52 +0200

    Disable logging in test env

--- lib/moderate_parameters/logger.rb
+++ lib/moderate_parameters/logger.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-$moderate_parameters_logger = Logger.new('log/moderate_parameters.log')
+unless Rails.env.test?
+  $moderate_parameters_logger = Logger.new('log/moderate_parameters.log')
 
-ActiveSupport::Notifications.subscribe('moderate_parameters') do |name, start, finish, id, payload|
-  $moderate_parameters_logger.info "#{payload[:controller]}##{payload[:action]} #{payload[:message]}"
+  ActiveSupport::Notifications.subscribe('moderate_parameters') do |name, start, finish, id, payload|
+    $moderate_parameters_logger.info "#{payload[:controller]}##{payload[:action]} #{payload[:message]}"
+  end
 end
